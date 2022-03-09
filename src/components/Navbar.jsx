4be821002ef16592b997551240ea578fde486c18
Karol Zyśk <trener.karolzysk@gmail.com>
2022-03-09 22:47:50 +0100

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const Navbar = () => {
-  return <div>Hello World of React</div>;
+  return <div>Hello World of React Elo</div>;
 };
 
 export default Navbar;
